b7b2d761b33792f6edb54e2980ac52a2ba042683
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-05 15:24:18 +0400

    Feat(pr.js): Change sturcture

--- pr.js
+++ pr.js
@@ -1,4 +1,12 @@
-const fib = (n) => n === 2 || n === 1 ? n - 1 : n === 0 ? 0 : fib(n - 1) + fib(n - 2);
+const fib = (n) => {
+  if(n === 2 || n === 1) {
+    return n - 1
+  } else if (n === 0) {
+    return 0 
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
+};
 
 
 console.log(fib(5))
